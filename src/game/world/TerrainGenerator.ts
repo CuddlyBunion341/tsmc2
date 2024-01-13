71d2bf7596d574204e4cf334550df5092fea9dab
Daniel Bengl <daniel.bengl@renuo.ch>
2024-01-13 23:26:32 +0100

    Update world generation parameters

--- src/game/world/TerrainGenerator.ts
+++ src/game/world/TerrainGenerator.ts
@@ -8,10 +8,11 @@ export class TerrainGenerator {
     this.noise2d = new FractalNoise2d(this.seed, 4)
     this.noise2d.frequencyX = 100
     this.noise2d.frequencyY = 100
+    this.noise2d.persistence = 2
   }
 
   public getBlock(x: number, y: number, z: number) {
-    const height = this.noise2d.get(x, z) * 10
+    const height = this.noise2d.get(x, z) * 20
 
     if (y > height) return blockIds.air
 
