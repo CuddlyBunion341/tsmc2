05c4e938df10686e2f5e9ffc95f3b5d522059ea7
Daniel Bengl <daniel.bengl@renuo.ch>
2024-01-20 00:23:10 +0100

    Remove unused code

--- src/game/world/ChunkMesher.ts
+++ src/game/world/ChunkMesher.ts
@@ -26,43 +26,10 @@ export class ChunkMesher {
     texture.unpackAlignment = 1
     texture.minFilter = THREE.NearestFilter
     texture.magFilter = THREE.NearestFilter
-    console.log(this.voxelData)
     texture.needsUpdate = true
-
-    const size = 16
-    // const buffer = new Float32Array(size * size)
-    // const view = new DataView(buffer)
-
-    const array = new Array(size * size * size)
-
-    {
-      let i = 0
-
-      for (let x = 0; x < 32; x++) {
-        for (let y = 0; y < 32; y++) {
-          for (let z = 0; z < 32; z++) {
-            i++
-            array[i] = Math.random() * 255
-          }
-        }
-      }
-    }
-
-    // const texture2d = new THREE.DataTexture(buffer, size, size, THREE.RedFormat)
-    const texture2d = new THREE.DataTexture(
-      new Uint8Array(array),
-      size,
-      size,
-      THREE.RedFormat
-    )
-    console.log(texture2d.image)
-    texture2d.needsUpdate = true
-
-    // const material = new THREE.MeshBasicMaterial()
 
     const material = voxelMaterial.clone()
     material.uniforms = {
-      uData: { value: texture2d },
       vData: { value: texture },
       chunkWidth: { value: this.width },
       chunkHeight: { value: this.height },
@@ -72,10 +39,8 @@ export class ChunkMesher {
       time: { value: 0 }
     }
 
-    const mesh = new THREE.Mesh(
-      new THREE.BoxGeometry(this.width, this.height, this.depth),
-      material
-    )
+    const geometry = new THREE.BoxGeometry(this.width, this.height, this.depth)
+    const mesh = new THREE.Mesh(geometry, material)
 
     return mesh
   }
--- src/game/world/raymarcher/shader.frag
+++ src/game/world/raymarcher/shader.frag
@@ -1,14 +1,9 @@
 // https://github.com/mrdoob/three.js/blob/master/examples/webgl2_volume_perlin.html
 
 varying vec2 v_uv;
-// uniform sampler2D uData;
 uniform highp sampler3D vData;
 
 void main() {
-  // float color = texture2D(uData, v_uv).r;
-  // float color = textureCube(vData, vec3(v_uv, 1.0)).r;
-  // float color = textureCube(vData, vec3(v_uv,0.0)).r;
-  // float color = texture(vData, vec3(v_uv,0.0)).r;
   float color = texture(vData, vec3(v_uv,1.0)).r;
   gl_FragColor = vec4(color, 0.0, 0.0, 1.0);
 }
