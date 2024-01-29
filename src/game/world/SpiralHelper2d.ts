858c48cedd02c938f473fafc2398998fbfc14f85
Daniel Bengl <daniel.bengl@renuo.ch>
2024-01-29 23:37:31 +0100

    Refactor helper

--- src/game/world/SpiralHelper2d.ts
+++ src/game/world/SpiralHelper2d.ts
@@ -4,6 +4,7 @@ export class SpiralHelper2d {
   public directionIndex: number
   public positions: Point2d[]
   public radius: number
+  public stepSize: number
 
   public static directions = [
     { x: 1, y: 0 },
@@ -19,15 +20,14 @@ export class SpiralHelper2d {
     this.directionIndex = 0
     this.positions = []
     this.radius = 0
+    this.stepSize = 1
   }
 
   generateSpiral() {
     let { x, y } = this.origin
-    let stepSize = 1
 
     while (this.radius < this.maxRadius) {
-      console.log({ stepSize, x, y, i: this.directionIndex, m5: this.directionIndex % 5 })
-      for (let i = 0; i < stepSize; i++) {
+      for (let i = 0; i < this.stepSize; i++) {
         this.positions.push({ x, y })
         x += SpiralHelper2d.directions[this.directionIndex % 4].x
         y += SpiralHelper2d.directions[this.directionIndex % 4].y
@@ -35,13 +35,8 @@ export class SpiralHelper2d {
 
       this.directionIndex++
 
-      if (this.directionIndex % 5 === 0) {
-        this.radius++
-      }
-
-      if (this.directionIndex % 2 === 0) {
-        stepSize++
-      }
+      if (this.directionIndex % 2 === 0) this.stepSize++
+      if (this.directionIndex % 5 === 0) this.radius++
     }
 
     return this.positions
