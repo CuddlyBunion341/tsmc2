0c6adc945aef97e6073d70f9004cbb61ac7958ff
Daniel Bengl <daniel.bengl@renuo.ch>
2024-01-29 22:58:13 +0100

    Refactor class

--- src/game/world/SpiralHelper2d.ts
+++ src/game/world/SpiralHelper2d.ts
@@ -1,21 +1,29 @@
+export type Point2d = { x: number; y: number }
+
 export class SpiralHelper2d {
-  constructor(public radius: number) { }
+  public directionIndex: number
+  public positions: Point2d[]
+
+  public static directions = [
+    { x: 1, y: 0 },
+    { x: 0, y: 1 },
+    { x: -1, y: 0 },
+    { x: 0, y: -1 }
+  ]
+
+  constructor(
+    public radius: number,
+    public origin: Point2d = { x: 0, y: 0 }
+  ) {
+    this.directionIndex = 0
+    this.positions = []
+  }
 
   generateSpiral() {
-    let x = 0
-    let y = 0
+    let { x, y } = this.origin
 
-    const directions = [
-      { x: 1, y: 0 },
-      { x: 0, y: 1 },
-      { x: -1, y: 0 },
-      { x: 0, y: -1 }
-    ]
+    // TODO: implement
 
-    const positions = []
-
-    // TODO: Implement
-
-    return positions
+    return this.positions
   }
 }
