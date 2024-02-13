2bd59bc98666b4c026837fd648f77e2e1345d272
Daniel Bengl <daniel.bengl@renuo.ch>
2024-02-13 16:10:06 +0100

    Refactor ShaderMaterial to RawShaderMaterial

--- src/game/world/ChunkMaterial.ts
+++ src/game/world/ChunkMaterial.ts
@@ -3,26 +3,37 @@ import * as THREE from "three"
 export class ChunkMaterial {
   private static material: THREE.ShaderMaterial
 
-  private static uniforms = {time: {value: 0.0}}
+  private static uniforms = { time: { value: 0.0 } }
 
   private static createMaterial() {
-      return new THREE.ShaderMaterial({
-        uniforms: this.uniforms,
-        vertexShader: `
-          varying vec2 vUv;
-          void main() {
-            vUv = uv;
-            vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
-            gl_Position = projectionMatrix * modelViewPosition; 
-          }
+    return new THREE.RawShaderMaterial({
+      uniforms: this.uniforms,
+      vertexShader: /*glsl*/`
+        uniform mat4 modelMatrix;
+        uniform mat4 modelViewMatrix;
+        uniform mat4 projectionMatrix;
+        uniform mat4 viewMatrix;
+        uniform mat3 normalMatrix;
+        uniform vec3 cameraPosition;
+        attribute vec3 position;
+        attribute vec3 normal;
+        attribute vec2 uv;
+
+        varying vec2 vUv;
+        void main() {
+          vUv = uv;
+          vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+          gl_Position = projectionMatrix * modelViewPosition; 
+        }
         `,
-        fragmentShader: `
+      fragmentShader: /*glsl*/`
+        precision highp float;
         varying vec2 vUv;
           void main() {
             gl_FragColor = vec4(vUv, 1.0, 1.0);
           }
         `
-      })
+    })
   }
 
   private static updateUniforms() {
