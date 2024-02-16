e555c11cc42dd881ccb727bb4472b1dfba5efcff
Daniel Bengl <daniel.bengl@renuo.ch>
2024-02-16 19:14:14 +0100

    Update chunk options

--- src/game/world/Chunk.ts
+++ src/game/world/Chunk.ts
@@ -2,7 +2,13 @@ import { ChunkData } from './ChunkData'
 import { ChunkMesher } from './ChunkMesher'
 import * as THREE from 'three'
 import { TerrainGenerator } from './TerrainGenerator'
-import { WaterMaterial } from './materials/WaterMaterial'
+
+export type ChunkOptions = {
+  terrainGenerator: TerrainGenerator
+  position: THREE.Vector3
+  dimensions: THREE.Vector3
+  materials: THREE.Material[]
+}
 
 export type ChunkMessageData = {
   position: {
@@ -22,6 +28,11 @@ export class Chunk {
   public readonly chunkData: ChunkData
   public readonly chunkMesher: ChunkMesher
   public readonly mesh: THREE.Mesh
+  public readonly terrainGenerator: TerrainGenerator
+  public readonly position: THREE.Vector3
+  public readonly dimensions: THREE.Vector3
+  public readonly materials: THREE.Material[]
+
 
   public static fromMessageData(data: ChunkMessageData) {
     const { position, dimensions, generatorSeed } = data
@@ -29,34 +40,39 @@ export class Chunk {
     const { width, height, depth } = dimensions
 
     const chunk = new Chunk(
-      new TerrainGenerator(generatorSeed),
-      new THREE.Vector3(x, y, z),
-      new THREE.Vector3(width, height, depth)
+      {
+        terrainGenerator: new TerrainGenerator(generatorSeed),
+        position: new THREE.Vector3(x, y, z),
+        dimensions: new THREE.Vector3(width, height, depth),
+        materials: []
+      }
     )
     return chunk
   }
 
   constructor(
-    public readonly terrainGenerator: TerrainGenerator,
-    public readonly position: THREE.Vector3,
-    public readonly dimensions: THREE.Vector3
+    options: ChunkOptions
   ) {
+    const { terrainGenerator, position, dimensions, materials } = options
+
+    this.terrainGenerator = terrainGenerator
+    this.position = position
+    this.dimensions = dimensions
+    this.materials = materials
+
     this.chunkData = new ChunkData(dimensions)
     this.chunkMesher = new ChunkMesher(dimensions, this.chunkData)
-    this.mesh = new THREE.Mesh(new THREE.BufferGeometry(), [
-      new THREE.MeshBasicMaterial({vertexColors: true}),
-      WaterMaterial.createMaterial(),
-    ])
+    this.mesh = new THREE.Mesh(new THREE.BufferGeometry(), this.materials)
     this.mesh.position.add(this.position.clone().multiply(this.dimensions))
   }
 
   generateTerrain() {
-    const chunkPosition = new THREE.Vector3(0,0,0)
+    const chunkPosition = new THREE.Vector3(0, 0, 0)
 
     for (let x = -1; x < this.dimensions.x + 1; x++) {
       for (let y = -1; y < this.dimensions.y + 1; y++) {
         for (let z = -1; z < this.dimensions.z + 1; z++) {
-          chunkPosition.set(x,y,z)
+          chunkPosition.set(x, y, z)
           const worldPosition = chunkPosition
             .clone()
             .add(this.position.clone().multiply(this.dimensions))
